6574be1a45b5a4fe58a189d99fd070ef1b4f7c4d
James Laverack <james.laverack@jetstack.io>
2022-05-27 14:18:54 +0100

    Support loading images from STDIN

--- pkg/image/image.go
+++ pkg/image/image.go
@@ -4,16 +4,47 @@ package image
 
 import (
 	"github.com/google/go-containerregistry/pkg/crane"
+	v1 "github.com/google/go-containerregistry/pkg/v1"
+	"io"
+	"io/ioutil"
 	"os"
 )
 
 func PullAndExport(imageName string, file *os.File) error {
-	img, err := crane.Pull(imageName)
-	if err != nil {
-		return err
+	var img v1.Image
+	if imageName == "-" {
+		t, err := ioutil.TempFile("", "paranoia_stdin")
+		if err != nil {
+			return err
+		}
+		defer func(f *os.File) {
+			err := f.Close()
+			if err != nil {
+				panic(err)
+			}
+			err = os.Remove(f.Name())
+			if err != nil {
+				panic(err)
+			}
+		}(t)
+
+		_, err = io.Copy(t, os.Stdin)
+		if err != nil {
+			return err
+		}
+		if err != nil {
+			return err
+		}
+		img, err = crane.Load(t.Name())
+	} else {
+		var err error
+		img, err = crane.Pull(imageName)
+		if err != nil {
+			return err
+		}
 	}
 
-	err = crane.Export(img, file)
+	err := crane.Export(img, file)
 	if err != nil {
 		return err
 	}
