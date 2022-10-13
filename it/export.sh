e1569e3456078dc62b0c324db124ecb8900cbcc1
James Laverack <james.laverack@jetstack.io>
2022-10-13 13:24:59 +0100

    Fix integration test

--- it/export.sh
+++ it/export.sh
@@ -10,7 +10,7 @@ docker save "${container_tag}" | paranoia export --output json - > /tmp/paranoia
 # The container-two-certs image contains the Let's Encrypt X1 root and the DigiCert Global root. Using the test command
 # and a bit of bash magic, we verfy that the JSON output has the correct SHA256 fingerprints in the correct places. It
 # could only have these fingerprints if it found the certs and exported them correctly.
-test "$(jq ".certificates[].FingerprintSHA256" -r /tmp/paranoia.json | sort | head -n1)" = "4348a0e9444c78cb265e058d5e8944b4d84f9662bd26db257f8934a443c70161"
-test "$(jq ".certificates[].FingerprintSHA256" -r /tmp/paranoia.json | sort | tail -n1)" = "96bcec06264976f37460779acf28c5a7cfe8a3c0aae11a8ffcee05c0bddf08c6"
+test "$(jq ".certificates[].fingerprintSHA256" -r /tmp/paranoia.json | sort | head -n1)" = "4348a0e9444c78cb265e058d5e8944b4d84f9662bd26db257f8934a443c70161"
+test "$(jq ".certificates[].fingerprintSHA256" -r /tmp/paranoia.json | sort | tail -n1)" = "96bcec06264976f37460779acf28c5a7cfe8a3c0aae11a8ffcee05c0bddf08c6"
 
 echo "Pass"
